a56e41f24ba0216bf8bee2d979715b5eee4d780e
shahidul5 <shahidul.cse46@gmail.com>
2025-04-12 23:44:13 +0600

    complete controller

---
+++ ControllerExample/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using ControllerExample.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("bookstore")]
+        public IActionResult Index(int? bookId)
+        {
+            if (bookId.HasValue == false)
+            {
+                return BadRequest("BookId is required");
+            }
+            if (bookId <= 0)
+            {
+                return BadRequest("BookId should be greater than 0");
+            }
+            return Json("shahid");
+        }
+        [Route("person")]
+        public JsonResult PersonDetails()
+        {
+            Person person = new Person() { Id = new Guid(), FirstName = "Shahidul", LastName = "Islam" };
+            return Json(person);
+        }
+    }
+}
---
+++ ControllerExample/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace ControllerExample.Models
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+    }
+}
